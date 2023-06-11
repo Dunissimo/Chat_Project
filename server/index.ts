72ac97fc72f095c7a4c102d39b27486cccfc66b5
Dunissimo <SuperDen214@yandex.ru>
2023-06-11 14:01:44 +0400

    start work on sockets

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,10 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import http from "http";
 import { router as authRouter } from "./routes/auth";
 import { errorMiddleware } from "./middlewares/error-middleware";
+import { Server } from "socket.io";
 
 dotenv.config();
 
@@ -15,11 +17,23 @@ app.use(express.json());
 app.use("/api", authRouter);
 app.use(errorMiddleware);
 
-// Routes
-app.get("/", (req: Request, res: Response) => {
-  res.send(`Express + TS server`);
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}!`);
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected!`);
+
+  io.on("disconnect", () => {
+    console.log(`${socket.id} disconnected!`);
+  });
+});
+
+server.listen(port, () => {
+  console.log("Server is running on", port);
 });
