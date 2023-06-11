0a6dc821654c8647bf1ed3cf49aa4be4c27014c3
Dunissimo <SuperDen214@yandex.ru>
2023-06-11 17:52:45 +0400

    log api url

--- client/src/api/api-routes.ts
+++ client/src/api/api-routes.ts
@@ -3,6 +3,8 @@ const API_BASE =
     ? `${import.meta.env.VITE_API_URL}:${import.meta.env.VITE_API_PORT}/api`
     : `${import.meta.env.VITE_PROD_API_URL}/api`;
 
+console.log(API_BASE);
+
 export const apiRoutes = {
   registerUser: API_BASE + "/auth/register/",
   loginUser: API_BASE + "/auth/login/",
